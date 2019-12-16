5bc665a6046e6eb3786e2b5e88bdd394439f1022
MRF1TR4/404 <58329775+FITRAKUN404@users.noreply.github.com>
2019-12-16 21:31:32 +0700

    Create KontolV1.sh

---
+++ KontolV1.sh
@@ -0,0 +1,8 @@
+figlet "kontol kau mau nge Record" |lolcat
+sleep 3
+figlet "Folder Ini Kosong" |lolcat
+sleep 3
+cowsay "KONTOL UDAH GAK ADA FOLDERNYA NIH KONTOL"|lolcat
+toilet "Error File Not Failed"|lolcat
+exit
+fi
